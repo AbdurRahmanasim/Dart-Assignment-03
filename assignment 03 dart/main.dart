d25bff7c6476d5604626e7bf2d1fb8338f723da1
ABDUR RAHMAN ASIM <92375077+AbdurRahmanasim@users.noreply.github.com>
2022-10-30 08:39:47 +0500

    Add files via upload

---
+++ assignment 03 dart/main.dart
@@ -0,0 +1,103 @@
+// void main() {
+ 
+//   QUESTION NO 1
+  
+//   List names = ["ali" , "ahmed" , "Kamran" , "Bilal"];
+//   print(names);
+  
+  
+//   QUESTION NO 2
+  
+//   List<String> days = [];
+//   days.addAll(["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]);
+//   print(days);
+  
+  
+  
+//   QUESTION NO 3
+  
+  
+//   List names = ["ali" , "Jaffar" , "Kamran" , "Bilal" , "Noman" , "Hunain" , "Waseem"];
+//   print(names.singleWhere((i) => i[0] == "a"));
+  
+  
+  
+//   QUESTION NO 4
+  
+  
+//   Map details = {
+//     'name' : "Abdur Rahman Asim",
+//     'address' : "Karachi",
+//     'age' : 19 ,
+//     'country' : 'Pakistan'
+//   };
+  
+//   details['country'] = "Australia" ;
+  
+//   print(details);
+  
+  
+  
+//   QUESTION NO 5
+  
+  
+//   Map details = {
+//     'name' : 'Abdur Rahman Asim',
+//     'phone' : "03245678934" ,
+//   };
+  
+  
+// details.forEach((key,val){
+  
+//   if(key.length ==  4){
+//     print(key);
+//   };
+  
+  
+// });
+
+  
+//   QUESTION NO 6
+   
+  
+//   var world = {
+//     'country' : "Pakistan",
+//     'capitalCity' : 'Islamabad',
+//     'currency' : "Rupee" ,
+//     'language' : 'Urdu',
+//   };
+  
+//  world.forEach((key,val){
+//    print(val);
+//  });
+  
+ 
+  
+//   QUESTION  NO 7
+  
+//   Map<String, double> mathMarks = {
+//   'ram': 30,
+//   'mark': 32,
+//   'harry': 88,
+//   'raj': 69,
+//   'john': 15,
+// };
+  
+//   mathMarks.removeWhere((key,val)=>val <= 30);
+  
+//   print(mathMarks);
+  
+//   QUESTION NO 8
+  
+//   Map<String, double> expenses = {
+//   'sun': 3000.0,
+//   'mon': 3000.0,
+//   'tue': 3234.0,
+// };
+  
+//   expenses['fri'] = 5000.0 ;
+  
+//   print(expenses);
+  
+  
+// }
